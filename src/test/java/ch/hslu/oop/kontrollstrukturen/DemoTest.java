67e65e17d456223a97e78302f43798eb9bcbe5ff
surfer412 <marcchardonnens@gmx.ch>
2020-10-27 14:50:07 +0100

    more unit tests

--- src/test/java/ch/hslu/oop/kontrollstrukturen/DemoTest.java
+++ src/test/java/ch/hslu/oop/kontrollstrukturen/DemoTest.java
@@ -11,15 +11,25 @@ class DemoTest {
     @Test
     void max() {
         assertEquals(5, demo.max(2,5));
+        assertEquals(96, demo.max(96,34));
+        assertEquals(22, demo.max(22,22));
+
     }
 
     @Test
     void min() {
         assertEquals(3, demo.min(3, 74));
+        assertEquals(58, demo.min(98, 58));
+        assertEquals(8, demo.min(8, 8));
     }
 
     @Test
     void testMax() {
         assertEquals( 9, demo.max(2,3,9));
+        assertEquals( 33, demo.max(22,33,2));
+        assertEquals( 21, demo.max(21,13,9));
+        assertEquals( 9, demo.max(3,3,9));
+        assertEquals( 9, demo.max(2,9,9));
+        assertEquals( 10, demo.max(10,10,10));
     }
 }
